92a2a7aa7f91ad1f3dd9d2f87314d204c88d4182
Sanidhya Desai <sanidhyadesai@Sanidhyas-MacBook-Pro.local>
2021-09-30 13:24:18 -0400

    update chat widget

--- src/app/customerservice/customerservice.component.ts
+++ src/app/customerservice/customerservice.component.ts
@@ -25,7 +25,8 @@ export class CustomerserviceComponent implements OnInit {
       w[x] =  w[x] || function() { (w[x].ac = w[x].ac || []).push(arguments) };
     })(window, document, 'amazon_connect', 'c0f781fa-99fa-47d4-8cc6-0c9c0b6a8b26');
     amazon_connect('styles', { openChat: { color: 'white', backgroundColor: '#123456'}, closeChat: { color: 'white', backgroundColor: '#123456'} });
-    amazon_connect('snippetId', 'QVFJREFIZ25JVzc0R0xQTVpWSlI0RjdyVlBselRRYnQzeW5TZEVnWGFmQnNNZ0lXbmdFNWNGYVZYQTlNVno5bW00ZngvSzV1QUFBQWJqQnNCZ2txaGtpRzl3MEJCd2FnWHpCZEFnRUFNRmdHQ1NxR1NJYjNEUUVIQVRBZUJnbGdoa2dCWlFNRUFTNHdFUVFNOHR4TDNxM2RzNk1sRGR6K0FnRVFnQ3RFMWw1NUhiYWlLNlNyL2FPRloyVEIxL0dic1pXdDlGdU4yVDZrNzJXTi9abnh3ZDltbjNKV0R1UXY6OnZNVUdqYUZQZGNTNHg3cTFlZDE2Y0lRaGZEcmF3Q1hrbHMwemlKTmFLOEJvV3hxZWZSYlhSemIxak5lMjFyYi8zUkdDbTJDYmdwZ2pCNlF6THYyQW90RFVLUndwZ0tFQzkxTllOVGM2QU9EUlc2NW5QYkVDa0JBYnBkSGpIenZhT3ZYNlZBVlFVaVE0OThzbER1MzBSYTBiQ1hEOUozTT0=');`;
+    amazon_connect('snippetId', 'QVFJREFIZ25JVzc0R0xQTVpWSlI0RjdyVlBselRRYnQzeW5TZEVnWGFmQnNNZ0lXbmdFNWNGYVZYQTlNVno5bW00ZngvSzV1QUFBQWJqQnNCZ2txaGtpRzl3MEJCd2FnWHpCZEFnRUFNRmdHQ1NxR1NJYjNEUUVIQVRBZUJnbGdoa2dCWlFNRUFTNHdFUVFNOHR4TDNxM2RzNk1sRGR6K0FnRVFnQ3RFMWw1NUhiYWlLNlNyL2FPRloyVEIxL0dic1pXdDlGdU4yVDZrNzJXTi9abnh3ZDltbjNKV0R1UXY6OnZNVUdqYUZQZGNTNHg3cTFlZDE2Y0lRaGZEcmF3Q1hrbHMwemlKTmFLOEJvV3hxZWZSYlhSemIxak5lMjFyYi8zUkdDbTJDYmdwZ2pCNlF6THYyQW90RFVLUndwZ0tFQzkxTllOVGM2QU9EUlc2NW5QYkVDa0JBYnBkSGpIenZhT3ZYNlZBVlFVaVE0OThzbER1MzBSYTBiQ1hEOUozTT0=');
+    `;
     this._renderer2.appendChild(this._document.body, script);
   }
 
